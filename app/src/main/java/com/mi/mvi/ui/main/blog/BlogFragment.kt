faa800bb15f3a2af97aab2d1b5e99d1226edeb72
MohamedIbrahim <mohamed_ibrahim15@hotmail.com>
2020-04-05 01:16:46 +0200

    searchView / swipe to refresh

--- app/src/main/java/com/mi/mvi/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/mi/mvi/ui/main/blog/BlogFragment.kt
@@ -1,11 +1,21 @@
 package com.mi.mvi.ui.main.blog
 
+import android.app.SearchManager
+import android.content.Context.SEARCH_SERVICE
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.mi.mvi.R
 import com.mi.mvi.data.models.BlogPost
 import com.mi.mvi.data.response_handler.DataState
@@ -19,14 +29,20 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 @ExperimentalCoroutinesApi
-class BlogFragment : BaseFragment(R.layout.fragment_blog), BlogListAdapter.Interaction {
+class BlogFragment : BaseFragment(R.layout.fragment_blog),
+    BlogListAdapter.Interaction,
+    SwipeRefreshLayout.OnRefreshListener {
 
     private val blogViewModel: BlogViewModel by sharedViewModel()
 
     private lateinit var recyclerAdapter: BlogListAdapter
+    private lateinit var searchView: SearchView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+        setHasOptionsMenu(true)
+        swipe_refresh.setOnRefreshListener(this)
         initRecyclerView()
         subscribeObservers()
         if (savedInstanceState == null) {
@@ -34,6 +50,16 @@ class BlogFragment : BaseFragment(R.layout.fragment_blog), BlogListAdapter.Inter
         }
     }
 
+    private fun onBlogSearchOrFilter() {
+        blogViewModel.loadFirstPage()
+        resetUI()
+    }
+
+    private fun resetUI() {
+        blog_post_recyclerview.smoothScrollToPosition(0)
+        dataStateChangeListener?.hideSoftKeyboard()
+        focusable_view.requestFocus()
+    }
 
     private fun subscribeObservers() {
         blogViewModel.dataState.observe(viewLifecycleOwner, Observer { dataState ->
@@ -43,7 +69,7 @@ class BlogFragment : BaseFragment(R.layout.fragment_blog), BlogListAdapter.Inter
 
         blogViewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
             viewState?.let {
-                if (blogViewModel.getPage() * PAGINATION_PAGE_SIZE > viewState.blogsFields.blogList.size){
+                if (blogViewModel.getPage() * PAGINATION_PAGE_SIZE > viewState.blogsFields.blogList.size) {
                     viewState.blogsFields.isQueryExhausted = true
                 }
                 recyclerAdapter.submitList(
@@ -52,6 +78,40 @@ class BlogFragment : BaseFragment(R.layout.fragment_blog), BlogListAdapter.Inter
                 )
             }
         })
+    }
+
+
+    private fun initSearchView(menu: Menu) {
+        activity?.apply {
+            val searchManager: SearchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+            searchView = menu.findItem(R.id.action_search).actionView as SearchView
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            searchView.maxWidth = Integer.MAX_VALUE
+            searchView.setIconifiedByDefault(true)
+            searchView.isSubmitButtonEnabled = true
+        }
+
+        //case 1 : ENTER ON ARROW
+        val searchPlate = searchView.findViewById(R.id.search_src_text) as EditText
+        searchPlate.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED
+                || actionId == EditorInfo.IME_ACTION_SEARCH
+            ) {
+                val searchQuery = v.text.toString()
+                Log.e("", searchQuery)
+                blogViewModel.setQuery(searchQuery)
+                onBlogSearchOrFilter()
+            }
+            true
+        }
+        //cas2: SEARCH BUTTON CLICKED (in toolbar)
+        (searchView.findViewById(R.id.search_go_btn) as View).setOnClickListener {
+            val searchQuery = searchPlate.text.toString()
+            Log.e("SEARCH_QUERY", searchQuery)
+            blogViewModel.setQuery(searchQuery)
+            onBlogSearchOrFilter()
+        }
+
     }
 
     private fun handlePagination(dataState: DataState<BlogViewState>?) {
@@ -115,4 +175,16 @@ class BlogFragment : BaseFragment(R.layout.fragment_blog), BlogListAdapter.Inter
         blogViewModel.setBlogPost(item)
         findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.search_menu, menu)
+        initSearchView(menu)
+    }
+
+    override fun onRefresh() {
+        onBlogSearchOrFilter()
+        swipe_refresh.isRefreshing = false
+    }
+
 }
